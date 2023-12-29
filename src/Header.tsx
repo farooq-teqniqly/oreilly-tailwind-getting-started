ca03b539a124ce16114364324d5b81bee8c15c01
farooq-teqniqly <farooq@teqniqly.com>
2023-12-29 13:38:57 -0800

    Use FontAwesome React library instead of css

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWind } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
   return (
     <div className={`container mx-auto`}>
       <header className={`flex justify-between items-center sticky top-0 z-10 py-4 bg-blue-900`}>
         <div className={`flex-shrink-0 ml-4 cursor-pointer`}>
-          <i className={`fas fa-wind fa-2x text-yellow-500`}></i>
+          <FontAwesomeIcon icon={faWind} className="fa-2x text-yellow-500" />
           <span className={`text-3xl font-semibold text-blue-200 pl-1`}>Tailwind School</span>
         </div>
         <ul className={`flex overflow-x-hidden mr-5 font-semibold`}>
