6b5cdd0ce284a9aaec0c693e7d6c68b9614be662
farooq-teqniqly <farooq@teqniqly.com>
2023-12-29 11:54:14 -0800

    Remove not needed files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
   return (
     <div className={`inline-flex flex-col text-left px-4 py-3`}>
-      React starter app styles with TailwindCSS.
+      React starter app styled with TailwindCSS.
     </div>
   );
 }
