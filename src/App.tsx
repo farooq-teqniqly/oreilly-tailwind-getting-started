8e22875d245edef494747d872fd137d904d7e03f
farooq-teqniqly <farooq@teqniqly.com>
2023-12-29 14:11:29 -0800

    Added FontAwesome React lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <div className={`inline-flex flex-col text-left px-4 py-3`}>
-      React starter app styled with TailwindCSS.
+      <div>React starter app styled with TailwindCSS</div>
+      <div>
+        <span>And FontAwesome icons.</span>
+        <FontAwesomeIcon icon={faCoffee} className={`pl-4 text-blue-200`}></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faCoffee} className={`pl-4 text-blue-400`}></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faCoffee} className={`pl-4 text-blue-800`}></FontAwesomeIcon>
+      </div>
     </div>
   );
 }
