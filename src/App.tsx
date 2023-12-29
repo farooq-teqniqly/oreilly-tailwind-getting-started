b4295bc6fca305d0c2dd05f80b5549623eb1e947
farooq-teqniqly <farooq@teqniqly.com>
2023-12-29 14:15:16 -0800

    Added FontAwesome React library

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { faWind } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
+  const fontWeights = [200, 400, 800];
+
   return (
     <div className={`inline-flex flex-col text-left px-4 py-3`}>
       <div>React starter app styled with TailwindCSS</div>
       <div>
         <span>And FontAwesome icons.</span>
-        <FontAwesomeIcon icon={faCoffee} className={`pl-4 text-blue-200`}></FontAwesomeIcon>
-        <FontAwesomeIcon icon={faCoffee} className={`pl-4 text-blue-400`}></FontAwesomeIcon>
-        <FontAwesomeIcon icon={faCoffee} className={`pl-4 text-blue-800`}></FontAwesomeIcon>
+        {fontWeights.map((w) => (
+          <FontAwesomeIcon
+            key={w}
+            icon={faWind}
+            className={`pl-4 text-blue-${w}`}
+          ></FontAwesomeIcon>
+        ))}
       </div>
     </div>
   );
